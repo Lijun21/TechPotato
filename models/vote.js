b93187fa76669d9cb141c11bdbcda5a7d4d8a189
Wang Lijun <wang283808065@gmail.com>
2018-04-10 19:51:56 -0700

    edit profile page

---
+++ models/vote.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var voteSchema = mongoose.Schema({
+    createdAt: { type: Date, default: Date.now},
+    upvoter: {
+        id: mongoose.Schema.Types.ObjectId,
+        username: String
+    }
+});
+
+module.exports = mongoose.model("vote", voteSchema);
