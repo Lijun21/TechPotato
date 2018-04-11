528c00d6c3600976ce9846cf72086e30b7b0746c
Wang Lijun <wang283808065@gmail.com>
2018-04-11 13:17:14 -0700

    delete landing page, fix facicon bug, clean up code

---
+++ models/project.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+//SCHEMA SETUP
+var projectSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    createdAt: { type: Date, default: Date.now},
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+    upvote: [
+        {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            upvoter: String
+        }
+    ]
+});
+
+//make a moudle that use the schema above that have bunch of methods on it
+module.exports = mongoose.model("Campground123", projectSchema);
---
+++ routes/projects.js
@@ -0,0 +1,87 @@
+var express = require("express");
+var router  = express.Router();
+var ProjectDB = require("../models/project");
+var Middleware = require("../middleware");
+
+//app.use("/projects", projectRoutes);
+
+//Index - show all projects
+router.get("/", function(req, res){
+    //get all project from DB
+    ProjectDB.find({}, function(err, allproject){
+        if (err){
+            console.log(err);
+        }else{
+            res.render("projects/index", {projects:allproject, page: 'projects'});
+        }
+    });
+});
+
+//GET/New - show from to create new project
+router.get("/new/", Middleware.isLoggedIn, function(req, res){
+    res.render("projects/new");
+})
+
+//POST/Create - add new project to DB
+router.post("/", Middleware.isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id : req.user._id,
+        username : req.user.username
+    }
+    var newproject = {name: name, image: image, description:desc, author: author};
+    ProjectDB.create(newproject, function(err, newlyCreated){
+        if (err || !newlyCreated){
+            console.log(err);
+        }else {
+            req.flash("success", "You new project has been Created =)");
+            res.redirect("/projects");
+        }
+    });
+});
+
+//GET/SHOW - shows more info about one project
+router.get("/:id", function(req, res){
+    ProjectDB.findById(req.params.id).populate("comments").exec(function(err, foundproject){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("projects/show", {project:foundproject});
+        }
+    });
+});
+
+//GET/Edit - show edit form for a project
+router.get("/:id/edit", Middleware.checkprojectOwnership, function(req, res){
+    ProjectDB.findById(req.params.id, function(err, foundproject){
+        res.render("projects/edit", {project: foundproject});
+    });
+});
+
+// PUT - update campgournd in the DB
+router.put("/:id", Middleware.checkprojectOwnership, function(req, res){
+    // find and update the correct project
+    ProjectDB.findByIdAndUpdate(req.params.id, req.body.project, function(err, updatedproject){
+       if(err || !updatedproject){
+           res.redirect("/projects");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/projects/" + req.params.id);
+       }
+    });
+});
+
+//DELETE - removes project and its comments from the database
+router.delete("/:id", Middleware.checkprojectOwnership, function(req, res){
+    ProjectDB.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/projects");
+        } else {
+            res.redirect("/projects");
+        }
+    })
+});
+
+module.exports = router;
