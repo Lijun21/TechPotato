34a0c5d2f373e858073978ed0213177fef158a28
Wang Lijun <wang283808065@gmail.com>
2018-04-25 21:26:21 -0700

    add upvoting function

--- routes/upvote.js
+++ routes/upvote.js
@@ -2,22 +2,24 @@ var express = require("express");
 var router  = express.Router();
 var ProjectDB = require("../models/project");
 var Upvote = require("../models/vote");
+var Middleware = require("../middleware");
 
-router.post("/projects/:_id/vote", function(req, res){
+router.post("/projects/:id/vote", Middleware.isLoggedIn, function(req, res){
     //find that projct
-    //add the username to vote username
+    //add the username to vote username array
     ProjectDB.findById(req.params.id, function(err, projectFound){
         if (err){
             req.flash("error", "Ops.. Can't find the data :(");
             console.log(err);
             res.redirect("/projects");
         }else{
+            // if you can find user in the vote user array, you can't keep vote
+            //if you can find, then create a new vote
             Upvote.create({}, function(err, vote){
                 if (err){
                     console.log(err);
                 }else{
                     // eval(require('locus'));
-                    //req.user = undefined??
                     vote.upvoter.id = req.user._id;
                     vote.upvoter.username = req.user.username;
                     vote.save();
